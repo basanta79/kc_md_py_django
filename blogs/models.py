616bce5b852f44d3b365d941f6f7f2e957b80b89
basanta79 <basanta79@gmail.com>
2019-05-12 08:50:03 +0200

    :construction: Adding more models

--- blogs/models.py
+++ blogs/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    cat_name = models.CharField(max_length=50)
+
 
 class Blog(models.Model):
     owner = models.CharField(max_length=100)
     title = models.CharField(max_length=50)
+    blog_image = models.FilePathField()
 
-    pass
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    intro = models.TextField(max_length=200)
+    body = models.TextField()
+    image = models.FilePathField()
+    date_time_pub = models.DateField(auto_now=True)
+    category = models.IntegerField()
