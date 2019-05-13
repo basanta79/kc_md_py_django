8d55c1ecea41596363138b2c66e3ecb33288388d
basanta79 <basanta79@gmail.com>
2019-05-13 23:16:57 +0200

    :construction: first migration

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blogs.models import Category, Blog, Post
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Blog)
+admin.site.register(Post)
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -5,10 +6,9 @@ class Category(models.Model):
 
 
 class Blog(models.Model):
-    owner = models.CharField(max_length=100)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     blog_image = models.FilePathField()
-
 
 
 class Post(models.Model):
@@ -17,4 +17,4 @@ class Post(models.Model):
     body = models.TextField()
     image = models.FilePathField()
     date_time_pub = models.DateField(auto_now=True)
-    category = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
